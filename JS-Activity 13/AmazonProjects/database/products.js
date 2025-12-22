18d0810f39e65815abc7b2afece2cf359a83bd10
Stewpth <xcalubs2311@gmail.com>
2025-12-22 11:57:10 +0800

    Make a template Data structure for products

---
+++ JS-Activity 13/AmazonProjects/database/products.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        ratings: {
+            stars: 4.5,
+            count: 87
+        },
+        priceCent: 1090
+    },
+    {
+        img: 'images/products/intermediate-composite-basketball.jpg',
+        name: 'Intermediate Size Basketball',
+        ratings: {
+            stars: 4,
+            count: 87
+        },
+        priceCent: 2095
+    },
+    {
+        img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+        ratings: {
+            stars: 4.5,
+            count: 56
+        },
+        priceCent: 1899
+    },
+    {
+        img: 'images/products/black-2-slot-toaster.jpg',
+        name: '2 Slot Toaster - Black',
+        ratings: {
+            stars: 5,
+            count: 2197
+        },
+        priceCents: 1899
+    }
+];

bbc6afd139b22593205bda75f940a4e0ae1c3a22
Stewpth <xcalubs2311@gmail.com>
2026-02-13 21:12:06 +0800

    14b-14c update the cart quantity when loading the page and clicking delete

--- JS-Activity 13/AmazonProjects/scripts/checkout.js
+++ JS-Activity 13/AmazonProjects/scripts/checkout.js
@@ -2,6 +2,7 @@ import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+updateCartQuantity(); // Call this function to update when loading the page
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -80,6 +81,17 @@ document.querySelectorAll('.js-delete-quantity-link')
 
             const productBox = document.querySelector(`.js-cart-item-box-${productId}`);
             productBox.remove();
+            updateCartQuantity();
         });
     });
 
+// We have this also in amazon.js and its okay because we avoid naming conflicts
+function updateCartQuantity() {
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-item-quantity').innerHTML = `${cartQuantity} items`;
+}

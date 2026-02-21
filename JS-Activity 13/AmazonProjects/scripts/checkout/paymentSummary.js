160826f821c1adc9340a2ea3a77e642e06d11db1
Stewpth <xcalubs2311@gmail.com>
2026-02-21 14:11:24 +0800

    Regenerate the paymentSummary instead use the DOM

--- JS-Activity 13/AmazonProjects/scripts/checkout/paymentSummary.js
+++ JS-Activity 13/AmazonProjects/scripts/checkout/paymentSummary.js
@@ -4,6 +4,7 @@ import { getDeliveryOption } from "../../data/deliveryOption.js";
 import { formatCurrency } from "../utils/money.js";
 
 export function updateSummaryPayment() {
+    let cartQuantity = 0;
     let productPriceCents = 0;
     let shippingCostCents = 0;
 
@@ -13,19 +14,52 @@ export function updateSummaryPayment() {
 
         const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
         shippingCostCents += deliveryOption.deliveryPriceCents; // Calculate total shipping cost based on delivery option
+
+        cartQuantity += cartItem.quantity; // Calculate total quantity of items in the cart
     });
 
+    const totalItems = cartQuantity;
     const totalBeforeTaxCents = productPriceCents + shippingCostCents;
     const taxCents = totalBeforeTaxCents * 0.1; // Assuming a tax rate of 10%
     const totalCents = totalBeforeTaxCents + taxCents;
+
 
     /* In the Javascript course, he generate all the codes from payment summary in the order summary, but I think it is better to get all the DOM elements related to
     payment summary in this file, so I can update the payment summary whenever there is a change in the order summary, such as quantity change, delivery option change, 
     or item deletion.*/
 
-    document.querySelector('.js-product-price').innerHTML = `$${formatCurrency(productPriceCents)}`;
-    document.querySelector('.js-shipping-cost').innerHTML = `$${formatCurrency(shippingCostCents)}`;
-    document.querySelector('.js-total-before-tax').innerHTML = `$${formatCurrency(totalBeforeTaxCents)}`;
-    document.querySelector('.js-tax').innerHTML = `$${formatCurrency(taxCents)}`;
-    document.querySelector('.js-total').innerHTML = `$${formatCurrency(totalCents)}`;
+    let paymentSummaryHTML =`
+        <div class="summary-order">
+            <h3 class="order-summary">Order Summary</h3>
+            <div class="payment-summary-row">
+                <div class="item-summary">Items (${totalItems}):</div>
+                <div class="payment-summary-money js-product-price">$${formatCurrency(productPriceCents)}</div>
+            </div>
+            <div class="payment-summary-row">
+                <span class="shipping-summary">Shipping & handling:</span>
+                <div class="payment-summary-money js-shipping-cost">$${formatCurrency(shippingCostCents)}</div>
+            </div>
+            <div class="payment-summary-row subtotal-row">
+                <span class="total-no-tax-summary subtotal-left-row">Total before tax:</span>
+                <div class="payment-summary-money subtotal-right-row js-total-before-tax">$${formatCurrency(totalBeforeTaxCents)}</div>
+            </div>
+            <div class="payment-summary-row">
+                <span>Estimated tax (10%):</span>
+                <div class="payment-summary-money js-tax">$${formatCurrency(taxCents)}</div>
+            </div>
+        </div>
+
+        <div class="total-summary-order">
+            <h3 class="total-cost">Order total:</h3>
+            <h3 class="total-cost js-total">$${formatCurrency(totalCents)}</h3>
+        </div>
+
+        <div class="place-order-box">
+            <button class="place-order-btn button-primary">
+                Place your order
+            </button>
+        </div>
+    `;
+
+    document.querySelector('.js-payment-info').innerHTML = paymentSummaryHTML;
 }

8cf22068b6baa04252c78bb8330d9be7dc86add3
Stewpth <xcalubs2311@gmail.com>
2026-03-12 17:16:10 +0800

    Move the jasmine testing framework to amazon project and add simple test for formatCurrency

---
+++ JS-Activity 13/AmazonProjects/test-simply/utils/moneyTest.js
@@ -0,0 +1,32 @@
+import { formatCurrency } from "../../scripts/utils/money.js";
+
+console.log('Test suite: formatCurrency');
+
+console.log('convert cent into dollars')
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('works with zero')
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('rounds to the nearest cent')
+if (formatCurrency(2000.5) === '20.01') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+console.log('rounds to 0 if the cents near to 0');
+
+if (formatCurrency(2000.4) === '20.00') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}

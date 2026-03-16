956589cfdf2ee14d2ba513f234b78890bc6e625f
Stewpth <xcalubs2311@gmail.com>
2026-03-16 22:38:18 +0800

    add a speed property then add go and brake method

--- JS-Activity 13/AmazonProjects/data/car.js
+++ JS-Activity 13/AmazonProjects/data/car.js
@@ -1,19 +1,45 @@
 class Car {
     brand;
     model;
+    speed;
 
     constructor(carDetails) {
         this.brand = carDetails.brand;
         this.model = carDetails.model;
+        this.speed = 0;
     }
 
     displayInfo() {
-        return `${this.brand} ${this.model}`
+        return `${this.brand} ${this.model}, Speed: ${this.speed} km/h`;
+    }
+
+    go() {
+        this.speed += 0;
+
+        if (this.speed > 200) {
+            this.speed = 200;
+        }
+    }
+
+    brake() {
+        this.speed -= 5;
+
+        if (this.speed < 0) {
+            this.speed = 0;
+        }
     }
 }
 
 const car1 = new Car({brand: 'Toyota', model: 'Corolla'});
 const car2 = new Car({brand: 'Tesla', model: 'Model 3'});
 
-console.log(car1.displayInfo());
+car1.go();
+car1.brake();
+car1.brake();
+console.log(car1.displayInfo());    
+
+car2.go();
+car2.go();
+car2.go();
+car2.go();
 console.log(car2.displayInfo());

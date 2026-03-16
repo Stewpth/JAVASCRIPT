206b7497c87715122b39faa4020cadc493bd30af
Stewpth <xcalubs2311@gmail.com>
2026-03-16 22:17:34 +0800

    17b. add a method called displayInfo()

--- JS-Activity 13/AmazonProjects/data/car.js
+++ JS-Activity 13/AmazonProjects/data/car.js
@@ -6,10 +6,14 @@ class Car {
         this.brand = carDetails.brand;
         this.model = carDetails.model;
     }
+
+    displayInfo() {
+        return `${this.brand} ${this.model}`
+    }
 }
 
 const car1 = new Car({brand: 'Toyota', model: 'Corolla'});
 const car2 = new Car({brand: 'Tesla', model: 'Model 3'});
 
-console.log(car1);
-console.log(car2);
+console.log(car1.displayInfo());
+console.log(car2.displayInfo());

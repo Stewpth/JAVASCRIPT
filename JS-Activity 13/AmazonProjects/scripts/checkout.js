e08b96019e3364c12cc0c3ad66568f7878913b48
Stewpth <xcalubs2311@gmail.com>
2026-03-25 09:07:04 +0800

    Add backend-practice file

--- JS-Activity 13/AmazonProjects/scripts/checkout.js
+++ JS-Activity 13/AmazonProjects/scripts/checkout.js
@@ -2,6 +2,7 @@ import { updateSummaryOrder } from "./checkout/orderSummary.js";
 import { updateSummaryPayment } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeaders.js";
 import {cart} from "./../data/cart-class.js"
+import "../data/backend-practice.js"
 
 updateSummaryOrder(cart);
 updateSummaryPayment();

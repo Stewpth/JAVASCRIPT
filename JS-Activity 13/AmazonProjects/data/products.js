cff91d0bdb90069ee1245c8d8394310379724e26
Stewpth <xcalubs2311@gmail.com>
2026-03-30 13:11:20 +0800

    add a products.json

--- JS-Activity 13/AmazonProjects/data/products.js
+++ JS-Activity 13/AmazonProjects/data/products.js
@@ -93,7 +93,7 @@ export function loadProducts(fun) {
             return new Product(productDetails);
         });
 
-        console.log('load products');
+        console.log(xhr.response);
         fun();
     });
 

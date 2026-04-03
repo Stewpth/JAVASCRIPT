b745762a68776c388aba2a5adc30e2ec337bb69f
Stewpth <xcalubs2311@gmail.com>
2026-04-03 22:59:37 +0800

    Make a short button for products, style it and make an smooth animation when interact with that button

--- JS-Activity 13/AmazonProjects/scripts/headers.js
+++ JS-Activity 13/AmazonProjects/scripts/headers.js
@@ -1,6 +1,20 @@
-const toggleBtn = document.querySelector('.js-hamburger-menu-toggle');
-const dropdown = document.querySelector('.js-hamburger-menu-dropdown');
+toggleMobileMenu();
+openShortMenu();
 
-toggleBtn.addEventListener('click', () => {
-    dropdown.classList.toggle('open');
-});
+function toggleMobileMenu() {
+    const toggleBtn = document.querySelector('.js-hamburger-menu-toggle');
+    const dropdown = document.querySelector('.js-hamburger-menu-dropdown');
+
+    toggleBtn.addEventListener('click', () => {
+        dropdown.classList.toggle('open');
+    });
+}
+
+function openShortMenu() {
+    const shortBtn = document.querySelector('.js-short-btn');
+    const shortDropDown = document.querySelector('.js-short-menu-dropdown');
+
+    shortBtn.addEventListener('click', () => {
+        shortDropDown.classList.toggle('open');
+    });
+}

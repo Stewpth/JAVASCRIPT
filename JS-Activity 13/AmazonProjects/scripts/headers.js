a1d45bd18b9a41b2c995516a839f97ec692d06af
Stewpth <xcalubs2311@gmail.com>
2026-04-05 22:31:40 +0800

    (fix bug) make the dropdown more interactive when interacting each of them

--- JS-Activity 13/AmazonProjects/scripts/headers.js
+++ JS-Activity 13/AmazonProjects/scripts/headers.js
@@ -5,8 +5,14 @@ function toggleMobileMenu() {
     const toggleBtn = document.querySelector('.js-hamburger-menu-toggle');
     const dropdown = document.querySelector('.js-hamburger-menu-dropdown');
 
+    const shortDropDown = document.querySelector('.js-short-menu-dropdown');
+
     toggleBtn.addEventListener('click', () => {
         dropdown.classList.toggle('open');
+
+        if (shortDropDown.classList.contains('open')) {
+            shortDropDown.classList.remove('open');
+        }
     });
 }
 
@@ -14,7 +20,13 @@ function openShortMenu() {
     const shortBtn = document.querySelector('.js-short-btn');
     const shortDropDown = document.querySelector('.js-short-menu-dropdown');
 
+    const dropdown = document.querySelector('.js-hamburger-menu-dropdown');
+
     shortBtn.addEventListener('click', () => {
         shortDropDown.classList.toggle('open');
+
+        if (dropdown.classList.contains('open')) {
+            dropdown.classList.remove('open');
+        }
     });
 }
